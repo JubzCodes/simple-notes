b79bc2293779a0339885aebc3f3b734ad268fbf6
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-09-04 18:41:47 -0400

    add note body

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrashCan,
+  faPenToSquare
+} from "@fortawesome/free-solid-svg-icons";
 
 const Note = () => {
   return (
-    <div>Note</div>
+    <div className='note'>
+      <span>Code an app today</span>
+      <div className='footer'>
+        <span>date</span>
+        <div className='buttons'>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrashCan}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
