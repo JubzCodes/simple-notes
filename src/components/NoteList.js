7ec67dbff752e252eeb6073a8d67c96920eb7815
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-09-04 18:18:03 -0400

    create note and note list components

---
+++ src/components/NoteList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Note from './Note'
+
+const NoteList = () => {
+  return (
+    <div>
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default NoteList
