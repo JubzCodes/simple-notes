b872bd13a1413b4fc4f2a84d712917ec3c7268c3
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-09-05 01:57:44 -0400

    add new note state

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,11 +1,20 @@
-import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const NewNote = () => {
+
+  //TEXT STATE
+  const [text, setText] = useState("");
+
+  //HANDLE TEXTAREA
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
   return (
     <div className='note new'>
-      <textarea rows='8' cols='10' placeholder='Add a new note :)' ></textarea>
+      <textarea rows='8' cols='10' placeholder='Add a new note :)' value={text} onChange={handleChange} ></textarea>
       <div className='footer'>
         <span>200 Remaining</span>
         <span className='save' title='Save'>
