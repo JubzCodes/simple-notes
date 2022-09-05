3a1738ff77bf1f64e7aaf5c30c00fa10dc6a5430
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-09-04 20:39:34 -0400

    css for search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import NoteList from './components/NoteList';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
+      <div className='search-bar'>
+        <span className='search-icon'>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </span>
+      <input type="text" placeholder='Search for Note...'/>
+      <span>
+        <FontAwesomeIcon icon={faTimes}/>
+      </span>
+      </div>
       <NoteList/>
     </div>
   );
